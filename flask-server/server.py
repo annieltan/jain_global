f11500306903d7aa8d3827cde1ca9e7a82f6301b
Annie Tan <annie.l.tan@gmail.com>
2025-01-18 23:58:40 -0500

    add prefix to routes and update CORS resource path

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Blueprint, Flask, jsonify
 from flask_cors import CORS
 
 load_dotenv() 
@@ -19,20 +19,23 @@ US_EXCHANGES = [
 ]
 
 app = Flask(__name__)
-CORS(app)
+stocks_bp = Blueprint('stocks', __name__, url_prefix='/api/v1')
+CORS(app, resources={r"/api/v1/*": {"origins": "https://jg-o41q.onrender.com/"}})
 
 
-@app.route("/stocks", methods=['GET'])
+@stocks_bp.route("/stocks", methods=['GET'])
 def get_stocks():
     stocks_url = f'https://financialmodelingprep.com/api/v3/stock/list?apikey={API_KEY}'
     stocks = requests.get(stocks_url).json()
     us_stocks = list(filter(lambda r: r['exchangeShortName'] in US_EXCHANGES and r['type'] == 'stock', stocks))
     return jsonify(us_stocks)
 
-@app.route("/historical-prices/<stock>", methods=['GET'])
+@stocks_bp.route("/historical-prices/<stock>", methods=['GET'])
 def get_historical_px(stock: str):
     px_url = f'https://financialmodelingprep.com/api/v3/historical-price-full/{stock}?apikey={API_KEY}'
     return requests.get(px_url).json()
+
+app.register_blueprint(stocks_bp)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
