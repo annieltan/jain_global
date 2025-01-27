8c8182e651a289c353b2a759561b2f09ba19487b
Annie Tan <annie.l.tan@gmail.com>
2025-01-27 13:53:52 -0500

    update readme, cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,42 @@
-import Dropdown from 'react-bootstrap/Dropdown';
 import React, { useState, useEffect, useRef } from 'react'
-import Highcharts from 'highcharts/highstock'
-import HighchartsReact from 'highcharts-react-official';
 
+import Dropdown from 'react-bootstrap/Dropdown'
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
 
 export default function App() {
-  // in future version, we could add a dropdown
-  // to choose other stocks with setActiveStock
+  // Future version: add a dropdown or freeform text input to support other tickers.
   const [activeStock, setActiveStock] = useState('AAPL')
-  const [activePriceType, setPriceType] = useState(null)
+  const [activeQuoteType, setPriceType] = useState(null)
   const [prices, setPrices] = useState({})
-
   const [chartOptions, setChartOptions] = useState({})
 
   const chartRef = useRef(null)
   const rectRef = useRef(null)
 
   const [isDragging, setIsDragging] = useState(false)
-  const [start, setStart] = useState({})
-  const [end, setEnd] = useState({})
+  const [startDrag, setStartDrag] = useState({})
+  const [endDrag, setEndDrag] = useState({})
   const [delta, setDelta] = useState(0)
 
   const handleMouseDown = (event) => {
-    // Only handle left mouse button
-    if (event.button !== 0) return;
+    const isLeftMouseButton = event.button === 0
+    const isDataPoint = event.target.className.baseVal.startsWith("highcharts-point")
 
-    // Ignore mousedown events outside of data points
-    if (!event.target.className.baseVal.startsWith("highcharts-point")) {
-      return
-    }
+    if (!isLeftMouseButton || !isDataPoint) return
 
     setIsDragging(true)
 
     const chart = chartRef.current.chart
-    // convert X pixels to X Axis value (milliseconds since epoch)
+    // Convert X pixels to X Axis value (milliseconds since epoch)
     const xValue = chart.xAxis[0].toValue(event.clientX)
-    // get data points that are currently visible
+    // Get data points that are currently visible
     const chartData = chartRef.current.chart.series[0].data
-    // since the X pixels don't give the exact value (close but not exact),
-    // I'm going to find the closest X value from chartData to get the Y value
+    // Since the X pixels don't give the exact value (close but not exact),
+    // find the closest X value from chartData to get the Y value
     chartData.forEach(data => {
       if (Math.abs(xValue - data.x)/data.x < 0.00001) {
-        setStart(data)
+        setStartDrag(data)
       }
     })
   }
@@ -49,46 +44,45 @@ export default function App() {
   const handleMouseMove = (event) => {
     if (isDragging) {
       if (rectRef.current) {
-        rectRef.current.destroy();
+        rectRef.current.destroy()
       }
 
       const chart = chartRef.current.chart
       const xAxis = chart.xAxis[0]
 
-      // convert X pixels to X Axis value (milliseconds since epoch)
+      // Convert X pixels to X Axis value (milliseconds since epoch)
       const xValue = xAxis.toValue(event.clientX)
       const chartData = chart.series[0].data
 
-      // determine direction that user is dragging in
-      const rightToLeft = (xValue > start.x)
+      // Determine direction that user is dragging in
+      const isRightToLeft = (xValue > startDrag.x)
 
-      // logic to mimick the default tooltip behavior
+      // Logic to mimic the default tooltip behavior
       // i.e. the end of the rectangle follows the tooltip
       for (let i = 0; i < chartData.length - 1; i++) {
         const current = chartData[i]
 
-        if (!current) continue;
-        // if (i + 1 >= chartData.length) break;
+        if (!current) continue
 
         const next = chartData[i + 1]
         const midpoint = current.x + (Math.abs(current.x - next.x) / 2)
 
-        if (rightToLeft && (xValue > midpoint && xValue < next.x)) {
-          setEnd(next)
-          setDelta((next.y - start.y).toFixed(2))
+        if (isRightToLeft && (xValue > midpoint && xValue < next.x)) {
+          setEndDrag(next)
+          setDelta((next.y - startDrag.y).toFixed(2))
           break
-        } else if (!rightToLeft && (xValue < midpoint && xValue > current.x)) {
-          setEnd(current)
-          setDelta((current.y - start.y).toFixed(2))
+        } else if (!isRightToLeft && (xValue < midpoint && xValue > current.x)) {
+          setEndDrag(current)
+          setDelta((current.y - startDrag.y).toFixed(2))
           break
         } else {
-          setEnd({x: xAxis.toValue(event.clientX)})
+          setEndDrag({x: xAxis.toValue(event.clientX)})
         }
       }
 
       // Calculate rectangle coordinates dynamically
-      const x1 = xAxis.toPixels(Math.min(start.x, end.x))
-      const x2 = xAxis.toPixels(Math.max(start.x, end.x))
+      const x1 = xAxis.toPixels(Math.min(startDrag.x, endDrag.x))
+      const x2 = xAxis.toPixels(Math.max(startDrag.x, endDrag.x))
 
       rectRef.current = chart.renderer
         .path([
@@ -103,17 +97,17 @@ export default function App() {
         })
         .add()
     }
-  };
+  }
 
   const handleMouseUp = (event) => {
     if (isDragging) {
-      setIsDragging(false);
+      setIsDragging(false)
       setDelta(0)
-      setStart({})
-      setEnd({})
+      setStartDrag({})
+      setEndDrag({})
 
       if (rectRef.current) {
-        rectRef.current.destroy();
+        rectRef.current.destroy()
       }
 
       const newOptions = {
@@ -123,32 +117,30 @@ export default function App() {
     }
   }
 
-  console.log('rendering')
-
   useEffect(() => {
-    fetch(`https://jain-global.onrender.com/api/v1/historical-prices/${activeStock}`).then(
+    fetch(`https://jg-o41q.onrender.com/api/v1/historical-prices/${activeStock}`).then(
     res => res.json()
     ).then(
     data => {
-        console.log('reformatting prices data after fetching')
-        const stockData = {}
+        const stockData = {
+          open: [],
+          close: [],
+          high: [],
+          low: [],
+          adjClose: [],
+          volume: [],
+          unadjustedVolume: [],
+          change: []
+        }
         // reformat data
         data['historical'].forEach(d => {
-          stockData['open'] = stockData['open'] || [];
           stockData['open'].push([d.date, d.open])
-          stockData['close'] = stockData['close'] || [];
           stockData['close'].push([d.date, d.close])
-          stockData['high'] = stockData['high'] || [];
           stockData['high'].push([d.date, d.high])
-          stockData['low'] = stockData['low'] || [];
           stockData['low'].push([d.date, d.low])
-          stockData['adjClose'] = stockData['adjClose'] || [];
           stockData['adjClose'].push([d.date, d.adjClose])
-          stockData['volume'] = stockData['volume'] || [];
           stockData['volume'].push([d.date, d.volume])
-          stockData['unadjustedVolume'] = stockData['unadjustedVolume'] || [];
           stockData['unadjustedVolume'].push([d.date, d.unadjustedVolume])
-          stockData['change'] = stockData['change'] || [];
           stockData['change'].push([d.date, d.change])
         })
         // Highcharts requires data to be sorted
@@ -209,37 +201,36 @@ export default function App() {
   }
 
   const onPriceTypeSelect = (eventKey, event) => {
-    event.preventDefault();
-    console.log(`Selected price type: ${eventKey}`);
+    event.preventDefault()
     setPriceType(eventKey)
-  };
+  }
 
-  // update chartOptions after setting activePriceType
+  // Update chartOptions after setting activeQuoteType
   useEffect(() => {
     const newOptions = {...options,
       series: {
         ...options.series,
-        data: prices[activePriceType],
+        data: prices[activeQuoteType],
       },
       subtitle: {
-        text: `${activePriceType} Prices`
+        text: `${activeQuoteType} Prices`
       },
     }
-    setChartOptions(newOptions);
-  }, [activePriceType]);
+    setChartOptions(newOptions)
+  }, [activeQuoteType])
 
-  // update chartOptions after every delta change
+  // Update chartOptions after every delta change
   useEffect(() => {
     const newOptions = {...options,
     }
-    setChartOptions(newOptions);
-  }, [delta]);
+    setChartOptions(newOptions)
+  }, [delta])
 
   return (
     <div>
       <Dropdown onSelect={onPriceTypeSelect}>
         <Dropdown.Toggle variant="success">
-        {activePriceType ? activePriceType : "Choose Price Type"}
+        {activeQuoteType ? activeQuoteType : "Choose Price Type"}
         </Dropdown.Toggle>
         <Dropdown.Menu>
           {['open', 'close', 'high', 'low', 'adjustedClose', 'volume', 'unadjustedVolume', 'change'].map((priceType) => (
@@ -249,7 +240,7 @@ export default function App() {
       </Dropdown>
 
       <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
-      {!activePriceType ? <></> :
+      {!activeQuoteType ? <></> :
         <HighchartsReact
           highcharts={Highcharts}
           options={chartOptions}
